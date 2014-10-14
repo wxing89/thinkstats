8c8a5f6e522069694a084701b4e14a57ced4d115
wxing89 <wxing89@163.com>
2014-10-14 17:37:12 +0800

    modified on Ubuntu

--- mine/_continuous.py
+++ mine/_continuous.py
@@ -1,1 +1,20 @@
-_continuous.py
+import erf
+import Cdf
+import cumulative
+import math
+import myplot
+import random
+import rankit
+import thinkstats
+import matplotlib.pyplot as pyplot
+
+def ExpoCdf(x, lam):
+    '''Evaluates CDF of the exponential distribution with parameter lam.'''
+    return 1 - math.exp(-lam * x)
+
+def ParetoCdf(x, alpha, xmin):
+    '''Evaluates CDF of the Pareto distribution with parameters alpha, xmin.'''
+    if x < xmin:
+        return 0
+    return 1 - pow(x / xmin, -alpha)
+
